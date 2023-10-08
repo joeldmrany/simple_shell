70e8753298e79b316468df825f8dc66482a66e5e
Muhammeddahman <Mohamedyasserdahman@gmail.com>
2023-10-08 22:27:09 +0300

    first push

---
+++ YM_string_functions4.c
@@ -0,0 +1,50 @@
+#include "shell.h"
+
+/**
+ * **strtow - convert the sentence into chars
+ * @str: a string
+ * @d: another string
+ * Return: pointer or NULL
+ */
+
+char **strtow(char *str, char *d)
+{
+	int a, b, c, e, numwords = 0;
+	char **s;
+
+	if (str == NULL || str[0] == 0)
+		return (NULL);
+	if (!d)
+		d = " ";
+	for (a = 0; str[a] != '\0'; a++)
+		if (!is_delim(str[a], d) && (is_delim(str[a + 1], d) || !str[a + 1]))
+			numwords++;
+
+	if (numwords == 0)
+		return (NULL);
+	s = malloc((1 + numwords) * sizeof(char *));
+	if (!s)
+		return (NULL);
+	for (a = 0, b = 0; b < numwords; b++)
+	{
+		while (is_delim(str[a], d))
+			a++;
+		c = 0;
+		while (!is_delim(str[a + c], d) && str[a + c])
+			c++;
+		s[b] = malloc((c + 1) * sizeof(char));
+		if (!s[b])
+		{
+			for (c = 0; c < b; c++)
+				free(s[c]);
+			free(s);
+			return (NULL);
+		}
+		for (e = 0; e < c; e++)
+			s[b][e] = str[a++];
+		s[b][e] = 0;
+	}
+	s[b] = NULL;
+	return (s);
+}
+
