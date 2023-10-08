948144d8de96076d727469f7016b660c5b04f9ee
Muhammeddahman <mohamedyasserdahman@gmail.com>
2023-10-08 22:31:00 +0300

    third push

--- YM_memory_functions.c
+++ YM_memory_functions.c
@@ -33,3 +33,33 @@ void ffree(char **pp)
 	free(t);
 }
 
+/**
+ * _realloc - it reallocs any memory
+ * @ptr: a pointer to it
+ * @old_size: size of bytes
+ * @new_size: new block size
+ *
+ * Return: the pointer
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *s;
+
+	if (!ptr)
+		return (malloc(new_size));
+	if (!new_size)
+		return (free(ptr), NULL);
+	if (new_size == old_size)
+		return (ptr);
+
+	s = malloc(new_size);
+	if (!s)
+		return (NULL);
+
+	old_size = old_size < new_size ? old_size : new_size;
+	while (old_size--)
+		s[old_size] = ((char *)ptr)[old_size];
+	free(ptr);
+	return (s);
+}
+
