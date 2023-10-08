75d7e2851e1d13ad795fe3fc96805b2aeca5f8e6
Muhammeddahman <Mohamedyasserdahman@gmail.com>
2023-10-08 22:28:08 +0300

    second push

--- YM_string_functions4.c
+++ YM_string_functions4.c
@@ -48,3 +48,48 @@ char **strtow(char *str, char *d)
 	return (s);
 }
 
+/**
+ * **strtow2 - converts the sentence into chars
+ * @str: a string
+ * @d: another char
+ * Return: pointer or NULL
+ */
+char **strtow2(char *str, char d)
+{
+	int a, b, c, e, numwords = 0;
+	char **s;
+
+	if (str == NULL || str[0] == 0)
+		return (NULL);
+	for (a = 0; str[a] != '\0'; a++)
+		if ((str[a] != d && str[a + 1] == d) ||
+		    (str[a] != d && !str[a + 1]) || str[a + 1] == d)
+			numwords++;
+	if (numwords == 0)
+		return (NULL);
+	s = malloc((1 + numwords) * sizeof(char *));
+	if (!s)
+		return (NULL);
+	for (a = 0, b = 0; b < numwords; b++)
+	{
+		while (str[a] == d && str[a] != d)
+			a++;
+		c = 0;
+		while (str[a + c] != d && str[a + c] && str[a + c] != d)
+			c++;
+		s[b] = malloc((c + 1) * sizeof(char));
+		if (!s[b])
+		{
+			for (c = 0; c < b; c++)
+				free(s[c]);
+			free(s);
+			return (NULL);
+		}
+		for (e = 0; e < c; e++)
+			s[b][e] = str[a++];
+		s[a][e] = 0;
+	}
+	s[a] = NULL;
+	return (s);
+}
+
