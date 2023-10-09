bc9aed86f1284f697a08f315f7e41ba13bf1acac
joeldmrany <joeldmrany238@gmail.com>
2023-10-09 14:42:54 +0300

    another one else

---
+++ YM_more_functions.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+
+
+
+/**
+ *_isalpha - checks if character is alpha
+ *@c: an input character
+ *Return: returns 1 or 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
