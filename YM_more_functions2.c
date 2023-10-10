c32c5ee4fd5348aa24d277ac82c6ea3329c2f53a
Muhammeddahman <mohamedyasserdahman@gmail.com>
2023-10-10 21:46:26 +0300

    a commit

---
+++ YM_more_functions2.c
@@ -0,0 +1,38 @@
+#include "shell.h"
+
+/**
+ * convert_number - it converts the function
+ * @num: the number
+ * @base: the base
+ * @flags: the flags
+ *
+ * Return: the sentence
+ */
+char *convert_number(long int num, int base, int flags)
+{
+	static char *array;
+	static char buffer[50];
+	char sign = 0;
+	char *ptr;
+	unsigned long n = num;
+
+	if (!(flags & CONVERT_UNSIGNED) && num < 0)
+	{
+		n = -num;
+		sign = '-';
+
+	}
+	array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
+	ptr = &buffer[49];
+	*ptr = '\0';
+
+	do	{
+		*--ptr = array[n % base];
+		n /= base;
+	} while (n != 0);
+
+	if (sign)
+		*--ptr = sign;
+	return (ptr);
+}
+
