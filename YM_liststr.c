e8fae120cddd11e530d93fe9b089c440e7f91a2a
joeldmrany <joeldmrany1@gmail.com>
2023-10-12 17:30:04 +0300

    list1, 3

--- YM_liststr.c
+++ YM_liststr.c
@@ -60,3 +60,36 @@ size_t print_list_str(const list_t *h)
 }
 
 
+/**
+ * add_node - node adder
+ * @head: about the head
+ * @str: node str
+ * @num: the number of them
+ *
+ * Return: list size
+ */
+list_t *add_node(list_t **head, const char *str, int num)
+{
+	list_t *new_head;
+
+	if (!head)
+		return (NULL);
+	new_head = malloc(sizeof(list_t));
+	if (!new_head)
+		return (NULL);
+	_memset((void *)new_head, 0, sizeof(list_t));
+	new_head->num = num;
+	if (str)
+	{
+		new_head->str = _strdup(str);
+		if (!new_head->str)
+		{
+			free(new_head);
+			return (NULL);
+		}
+	}
+	new_head->next = *head;
+	*head = new_head;
+	return (new_head);
+}
+
