62c2402e8105527051636f588a13cd51947fc197
joeldmrany <joeldmrany1@gmail.com>
2023-10-12 17:28:20 +0300

    list1

---
+++ YM_liststr.c
@@ -0,0 +1,42 @@
+#include "shell.h"
+/**
+ * delete_node_at_index - it deletes not needed node
+ * @head: pointer of the head
+ * @index: delete node number
+ *
+ * Return: 0 or 1
+ */
+int delete_node_at_index(list_t **head, unsigned int index)
+{
+	list_t *node, *prev_node;
+	unsigned int i = 0;
+
+	if (!head || !*head)
+		return (0);
+
+	if (!index)
+	{
+		node = *head;
+		*head = (*head)->next;
+		free(node->str);
+		free(node);
+		return (1);
+	}
+	node = *head;
+	while (node)
+	{
+		if (i == index)
+		{
+			prev_node->next = node->next;
+			free(node->str);
+			free(node);
+			return (1);
+		}
+		i++;
+		prev_node = node;
+		node = node->next;
+	}
+	return (0);
+}
+
+
