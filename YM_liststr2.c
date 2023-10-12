238494f712e2b03e6fcbb51957129ef4dafea1b5
Muhammeddahman <mohamedyasserdahman@gmail.com>
2023-10-12 17:44:44 +0300

    list2, 1

--- YM_liststr2.c
+++ YM_liststr2.c
@@ -20,4 +20,39 @@ ssize_t get_node_index(list_t *head, list_t *node)
 	}
 	return (-1);
 }
+/**
+ * list_to_strings - gets the string array
+ * @head: first null pointer
+ *
+ * Return: string array
+ */
+char **list_to_strings(list_t *head)
+{
+	list_t *node = head;
+	size_t i = list_len(head), j;
+	char **strs;
+	char *str;
+
+	if (!head || !i)
+		return (NULL);
+	strs = malloc(sizeof(char *) * (i + 1));
+	if (!strs)
+		return (NULL);
+	for (i = 0; node; node = node->next, i++)
+	{
+		str = malloc(_strlen(node->str) + 1);
+		if (!str)
+		{
+			for (j = 0; j < i; j++)
+				free(strs[j]);
+			free(strs);
+			return (NULL);
+		}
+
+		str = _strcpy(str, node->str);
+		strs[i] = str;
+	}
+	strs[i] = NULL;
+	return (strs);
+}
 
