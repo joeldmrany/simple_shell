0af528ad0f2928ac5f24ebce15a6b293e3e6f879
joeldmrany <joeldmrany1@gmail.com>
2023-10-12 17:31:05 +0300

    list1, 4

--- YM_liststr.c
+++ YM_liststr.c
@@ -115,4 +115,44 @@ void free_list(list_t **head_ptr)
 	}
 	*head_ptr = NULL;
 }
+/**
+ * add_node_end - end node
+ * @head: head of the node
+ * @str: node string
+ * @num: the index of the node
+ *
+ * Return: list size
+ */
+list_t *add_node_end(list_t **head, const char *str, int num)
+{
+	list_t *new_node, *node;
+
+	if (!head)
+		return (NULL);
+
+	node = *head;
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
+		return (NULL);
+	_memset((void *)new_node, 0, sizeof(list_t));
+	new_node->num = num;
+	if (str)
+	{
+		new_node->str = _strdup(str);
+		if (!new_node->str)
+		{
+			free(new_node);
+			return (NULL);
+		}
+	}
+	if (node)
+	{
+		while (node->next)
+			node = node->next;
+		node->next = new_node;
+	}
+	else
+		*head = new_node;
+	return (new_node);
+}
 
