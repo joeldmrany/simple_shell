624b8d92749fb56df3d5187807e32dcb696332c3
joeldmrany <joeldmrany1@gmail.com>
2023-10-14 20:43:05 +0300

    info 2

--- YM_info.c
+++ YM_info.c
@@ -61,3 +61,15 @@ void set_info(info_t *info, char **av)
 	}
 }
 
+/**
+ * clear_info - it clears the info to struct
+ * @info: the address of structure
+ */
+void clear_info(info_t *info)
+{
+	info->arg = NULL;
+	info->argv = NULL;
+	info->path = NULL;
+	info->argc = 0;
+}
+
