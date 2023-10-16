2d60ff186aa0c6f864d7efe449fd0febf7d6785e
joeldmrany <joeldmrany1@gmail.com>
2023-10-16 18:01:50 +0300

    env2 : 2

--- YM_env2.c
+++ YM_env2.c
@@ -73,3 +73,19 @@ int _unsetenv(info_t *info, char *var)
 	return (info->env_changed);
 }
 
+/**
+ * get_environ - a string copy returns
+ * @info: info const
+ * Return: 0
+ */
+char **get_environ(info_t *info)
+{
+	if (!info->environ || info->env_changed)
+	{
+		info->environ = list_to_strings(info->env);
+		info->env_changed = 0;
+	}
+
+	return (info->environ);
+}
+
