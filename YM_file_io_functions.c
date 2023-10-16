0b83078ad02d6f8f7e14cb4371f0c71ec266a994
Muhammeddahman <mohamedyasserdahman@gmail.com>
2023-10-16 18:14:29 +0300

    file_io : 2

--- YM_file_io_functions.c
+++ YM_file_io_functions.c
@@ -53,4 +53,52 @@ int write_history(info_t *info)
 	return (1);
 }
 
+/**
+ * read_history - file history read
+ * @info: struct parameter
+ *
+ * Return: 0 or 1
+ */
+int read_history(info_t *info)
+{
+	int a, end = 0, count = 0;
+	ssize_t fd, rdlen, fsize = 0;
+	struct stat st;
+	char *buf = NULL, *filename = get_history_file(info);
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	free(filename);
+	if (fd == -1)
+		return (0);
+	if (!fstat(fd, &st))
+		fsize = st.st_size;
+	if (fsize < 2)
+		return (0);
+	buf = malloc(sizeof(char) * (fsize + 1));
+	if (!buf)
+		return (0);
+	rdlen = read(fd, buf, fsize);
+	buf[fsize] = 0;
+	if (rdlen <= 0)
+		return (free(buf), 0);
+	close(fd);
+	for (a = 0; a < fsize; a++)
+		if (buf[a] == '\n')
+		{
+			buf[a] = 0;
+			build_history_list(info, buf + end, count++);
+			end = a + 1;
+		}
+	if (end != a)
+		build_history_list(info, buf + end, count++);
+	free(buf);
+	info->histcount = count;
+	while (info->histcount-- >= HIST_MAX)
+		delete_node_at_index(&(info->history), 0);
+	renumber_history(info);
+	return (info->histcount);
+}
 
