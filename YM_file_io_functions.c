e80d4f613889feea49a129c953e21cbd659872b2
Muhammeddahman <mohamedyasserdahman@gmail.com>
2023-10-16 18:13:17 +0300

    file_io : 1

--- YM_file_io_functions.c
+++ YM_file_io_functions.c
@@ -24,4 +24,33 @@ char *get_history_file(info_t *info)
 	return (buf);
 }
 
+/**
+ * write_history - append or creat file history
+ * @info: structure of parameter
+ *
+ * Return: 1 or -1
+ */
+int write_history(info_t *info)
+{
+	ssize_t fd;
+	char *filename = get_history_file(info);
+	list_t *node = NULL;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0644);
+	free(filename);
+	if (fd == -1)
+		return (-1);
+	for (node = info->history; node; node = node->next)
+	{
+		_putsfd(node->str, fd);
+		_putfd('\n', fd);
+	}
+	_putfd(BUF_FLUSH, fd);
+	close(fd);
+	return (1);
+}
+
 
