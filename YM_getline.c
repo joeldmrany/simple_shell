5d869f06afa1fa992a18281e6a6e3a7cd788eedf
joeldmrany <joeldmrany1@gmail.com>
2023-10-17 18:16:01 +0300

    line : 1

--- YM_getline.c
+++ YM_getline.c
@@ -12,4 +12,25 @@ void sigintHandler(__attribute__((unused))int sig_num)
 	_puts("$ ");
 	_putchar(BUF_FLUSH);
 }
+/**
+ * read_buf - the buffer
+ *
+ * @info: parameter structure
+ *
+ * @buf: buffering it
+ * @i: size of it
+ *
+ * Return: the char
+ */
+ssize_t read_buf(info_t *info, char *buf, size_t *i)
+{
+	ssize_t r = 0;
+
+	if (*i)
+		return (0);
+	r = read(info->readfd, buf, READ_BUF_SIZE);
+	if (r >= 0)
+		*i = r;
+	return (r);
+}
 
