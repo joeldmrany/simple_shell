575745d9d2fee65a8a482b3b2b58e96540956d55
Muhammeddahman <mohamedyasserdahman@gmail.com>
2023-10-17 18:33:10 +0300

    hsh : 0

---
+++ YM_hsh.c
@@ -0,0 +1,43 @@
+#include "shell.h"
+
+/**
+ * fork_cmd - having it
+ *
+ * @info: information parameters
+ *
+ * Return: (void)
+ */
+void fork_cmd(info_t *info)
+{
+	pid_t child_pid;
+
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		/* TODO: ERRORING */
+		perror("Error:");
+		return;
+	}
+	if (child_pid == 0)
+	{
+		if (execve(info->path, info->argv, get_environ(info)) == -1)
+		{
+			free_info(info, 1);
+			if (errno == EACCES)
+				exit(126);
+			exit(1);
+		}
+		/* TODO: SEMI ERROR */
+	}
+	else
+	{
+		wait(&(info->status));
+		if (WIFEXITED(info->status))
+		{
+			info->status = WEXITSTATUS(info->status);
+			if (info->status == 126)
+				print_error(info, "Permission denied\n");
+		}
+	}
+}
+
