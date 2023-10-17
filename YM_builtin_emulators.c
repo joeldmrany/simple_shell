b7fc75d38cae10d7b00a7c67fdb1799618d48178
joeldmrany <joeldmrany1@gmail.com>
2023-10-17 19:43:10 +0300

    builtin_emulator0 : 0

---
+++ YM_builtin_emulators.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+
+/**
+ * _myhelp - get the help
+ * @info: containing argument structure
+ *  Return: 0
+ */
+int _myhelp(info_t *info)
+{
+	char **arg_array;
+
+	arg_array = info->argv;
+	_puts("help call works. Function not yet implemented \n");
+	if (0)
+		_puts(*arg_array);
+	return (0);
+}
+
