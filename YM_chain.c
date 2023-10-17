6bb3a6b7d02ac78a3166c412fc0efc793d0f8d95
joeldmrany <joeldmrany1@gmail.com>
2023-10-17 18:07:18 +0300

    chain : 3

--- YM_chain.c
+++ YM_chain.c
@@ -78,4 +78,38 @@ int is_chain(info_t *info, char *buf, size_t *p)
 	*p = j;
 	return (1);
 }
+/**
+ * check_chain - the chain of checker
+ * @info: parameter structure
+ * @buf: char buffer
+ * @p: string like it
+ * @i: buffering it
+ * @len: the length of power
+ *
+ * Return: (void)
+ */
+void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
+{
+	size_t j = *p;
+
+	if (info->cmd_buf_type == CMD_AND)
+	{
+		if (info->status)
+		{
+			buf[i] = 0;
+			j = len;
+		}
+	}
+	if (info->cmd_buf_type == CMD_OR)
+	{
+		if (!info->status)
+		{
+			buf[i] = 0;
+			j = len;
+		}
+	}
+
+	*p = j;
+}
+
 
