62daa57b582ef0611988b6e3171b3af95b7bdb4b
Muhammeddahman <mohamedyasserdahman@gmail.com>
2023-10-17 19:28:04 +0300

    string functions1 : 0

---
+++ YM_string_functions2.c
@@ -0,0 +1,23 @@
+#include "shell.h"
+
+/**
+ * _putchar - get the char from input to output
+ * @c: printed char
+ *
+ * Return: 1 or -1 
+ */
+int _putchar(char c)
+{
+	static int a;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || a >= WRITE_BUF_SIZE)
+	{
+		write(1, buf, a);
+		a = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[a++] = c;
+	return (1);
+}
+
