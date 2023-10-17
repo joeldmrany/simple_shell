81a68c8fcfdb73ab39c35da71451e5f021192201
joeldmrany <joeldmrany1@gmail.com>
2023-10-17 18:19:00 +0300

    line : 4

--- YM_getline.c
+++ YM_getline.c
@@ -121,4 +121,50 @@ ssize_t get_input(info_t *info)
 	*buf_p = buf;
 	return (r);
 }
+/**
+ * _getline - having another one
+ * @info: parameter structure
+ * @ptr: address pointer
+ * @length: any thing
+ *
+ * Return: the char
+ */
+int _getline(info_t *info, char **ptr, size_t *length)
+{
+	static char buf[READ_BUF_SIZE];
+	static size_t i, len;
+	size_t k;
+	ssize_t r = 0, s = 0;
+	char *p = NULL, *new_p = NULL, *c;
+
+	p = *ptr;
+	if (p && length)
+		s = *length;
+	if (i == len)
+		i = len = 0;
+
+	r = read_buf(info, buf, &len);
+	if (r == -1 || (r == 0 && len == 0))
+		return (-1);
+
+	c = _strchr(buf + i, '\n');
+	k = c ? 1 + (unsigned int)(c - buf) : len;
+	new_p = _realloc(p, s, s ? s + k : k + 1);
+	if (!new_p)
+		return (p ? free(p), -1 : -1);
+
+	if (s)
+		_strncat(new_p, buf + i, k - i);
+	else
+		_strncpy(new_p, buf + i, k - i + 1);
+
+	s += k - i;
+	i = k;
+	p = new_p;
+
+	if (length)
+		*length = s;
+	*ptr = p;
+	return (s);
+}
 
