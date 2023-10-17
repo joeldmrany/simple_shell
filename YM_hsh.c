02be0e672e3b0243acf330147940bc06a75efa25
Muhammeddahman <mohamedyasserdahman@gmail.com>
2023-10-17 18:33:34 +0300

    hsh : 1

--- YM_hsh.c
+++ YM_hsh.c
@@ -40,4 +40,47 @@ void fork_cmd(info_t *info)
 		}
 	}
 }
+/**
+ * hsh - mains loop
+ *
+ * @info: parameter return value
+ * @av: vectorial argument
+ *
+ * Return: 1 or 0
+ */
+int hsh(info_t *info, char **av)
+{
+	ssize_t r = 0;
+	int retur = 0;
+
+	while (r != -1 && retur != -2)
+	{
+		clear_info(info);
+		if (interactive(info))
+			_puts("$ ");
+		_eputchar(BUF_FLUSH);
+		r = get_input(info);
+		if (r != -1)
+		{
+			set_info(info, av);
+			retur = find_builtin(info);
+			if (retur == -1)
+				find_cmd(info);
+		}
+		else if (interactive(info))
+			_putchar('\n');
+		free_info(info, 0);
+	}
+	write_history(info);
+	free_info(info, 1);
+	if (!interactive(info) && info->status)
+		exit(info->status);
+	if (retur == -2)
+	{
+		if (info->err_num == -1)
+			exit(info->status);
+		exit(info->err_num);
+	}
+	return (retur);
+}
 
