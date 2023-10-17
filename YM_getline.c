cfc29d88d2e7a506bf7e3f17a9ebfed1287bc638
joeldmrany <joeldmrany1@gmail.com>
2023-10-17 18:16:22 +0300

    line : 2

--- YM_getline.c
+++ YM_getline.c
@@ -34,3 +34,46 @@ ssize_t read_buf(info_t *info, char *buf, size_t *i)
 	return (r);
 }
 
+/**
+ * input_buf - buffering command
+ *
+ * @info: parameter structure
+ * @buf: address buffering
+ * @len: address var
+ *
+ * Return: bytes number
+ */
+ssize_t input_buf(info_t *info, char **buf, size_t *len)
+{
+	ssize_t r = 0;
+	size_t len_p = 0;
+
+	if (!*len)
+	{
+		free(*buf);
+		*buf = NULL;
+		signal(SIGINT, sigintHandler);
+#if USE_GETLINE
+		r = getline(buf, &len_p, stdin);
+#else
+		r = _getline(info, buf, &len_p);
+#endif
+		if (r > 0)
+		{
+			if ((*buf)[r - 1] == '\n')
+			{
+				(*buf)[r - 1] = '\0';
+				r--;
+			}
+			info->linecount_flag = 1;
+			remove_comments(*buf);
+			build_history_list(info, *buf, info->histcount++);
+			{
+				*len = r;
+				info->cmd_buf = buf;
+			}
+		}
+	}
+	return (r);
+}
+
